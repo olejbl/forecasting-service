d273ff03d58de6703749fedc15dc35de92b716ec
olejbl <38566764+olejbl@users.noreply.github.com>
2023-06-14 21:01:12 +0200

    add slug route and fetch utils

--- forecast-app/app/location/[slug]/page.tsx
+++ forecast-app/app/location/[slug]/page.tsx
@@ -1,3 +1,62 @@
+"use client"
+
+import { GeocodingResponse, fetchGeoCoding } from "@/lib/utils"
+import { useState, useEffect } from "react"
+
 export default function Page({ params }: { params: { slug: string } }) {
-  return <div>City: {params.slug}</div>
+  const [weatherData, setWeatherData] = useState<GeocodingResponse | null>(null)
+
+  useEffect(() => {
+    const city = params.slug
+
+    fetchGeoCoding(city)
+      .then((data) => setWeatherData(data))
+      .catch((err) => console.error(err))
+  }, [params.slug])
+
+  if (!weatherData) {
+    return <div>Loading...</div>
+  }
+  console.log(weatherData)
+  return (
+    <div>
+      <h1 className="font-bold uppercase">{params.slug}</h1>
+      <div className="grid grid-cols-1 grid-rows-2 place-items-center md:grid-cols-2 md:grid-rows-1">
+        <div className="col-span-1 grid grid-cols-2 grid-rows-4 place-items-center">
+          <h3 className="col-span-2">{weatherData.weather[0].main}</h3>
+          <h2 className="col-span-2 row-span-2 text-2xl font-semibold">
+            {weatherData.main.temp}°C
+          </h2>
+          <h3 className="px-4">H:{weatherData.main.temp_max}°</h3>
+          <h3 className="px-4">L:{weatherData.main.temp_min}°</h3>
+        </div>
+        <div className="col-span-1 mt-4 grid grid-cols-2 grid-rows-2">
+          <div className="grid grid-flow-row grid-rows-2 border-b-2 border-r-2">
+            <p className="px-4">Sunrise</p>
+            <p className="px-4">
+              {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString()}
+            </p>
+          </div>
+          <div className="grid grid-flow-row grid-rows-2 border-b-2 border-l-2">
+            <p className="px-4">Sunset</p>
+            <p className="px-4">
+              {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString()}
+            </p>
+          </div>
+          <div className="grid grid-flow-row grid-rows-2 border-r-2 border-t-2">
+            <p className="px-4">Humidity</p>
+            <p className="px-4">
+              {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString()}
+            </p>
+          </div>
+          <div className="grid grid-flow-row grid-rows-2 border-l-2 border-t-2">
+            <p className="px-4">Visibility</p>
+            <p className="px-4">
+              {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString()}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
--- forecast-app/lib/utils.ts
+++ forecast-app/lib/utils.ts
@@ -4,3 +4,71 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export type GeocodingResponse = {
+  coord: {
+    lon: number
+    lat: number
+  }
+  weather: [
+    {
+      id: number
+      main: string
+      description: string
+      icon: string
+    }
+  ]
+  base: string
+  main: {
+    temp: number
+    feels_like: number
+    pressure: number
+    humidity: number
+    temp_min: number
+    temp_max: number
+    sea_level?: number
+    grnd_level?: number
+  }
+  visibility: number
+  wind: {
+    speed: number
+    deg: number
+    gust?: number
+  }
+  clouds: {
+    all: number
+  }
+  rain?: {
+    "1h"?: number
+    "3h"?: number
+  }
+  snow?: {
+    "1h"?: number
+    "3h"?: number
+  }
+  dt: number
+  sys: {
+    type: number
+    id: number
+    message: number
+    country: string
+    sunrise: number
+    sunset: number
+  }
+  timezone: number
+  id: number
+  name: string
+  cod: number
+}
+
+// Helper function to fetch the lat,lon coordinates
+export async function fetchGeoCoding(city: string) {
+  const API_KEY = "b1d0ced0452ac6963b5ca1e3cb0bc34e"
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+  ) //fetch using the geocoding-api
+  const data: GeocodingResponse = await response.json()
+  console.log(data)
+
+  return data
+}
